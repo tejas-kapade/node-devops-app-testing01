5cc416f57cd4eafaabed4425d1fe4a19caab8fbe
Tejas Kapade <me.tejaskapade@gmail.com>
2026-03-13 14:30:57 +0530

    AddedDocker Compose with MongoDB

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 const PORT = 3000;
+
+// MongoDB connection
+mongoose.connect("mongodb://mongo:27017/devopsdb")
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello from DevOps Node.js App 🚀");
