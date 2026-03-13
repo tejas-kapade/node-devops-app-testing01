b8a3bf5e9202b93ae546ae6b9eeac318e934ed69
Tejas Kapade <me.tejaskapade@gmail.com>
2026-03-13 15:01:27 +0530

    Added healthcheck and restart policy

--- Dockerfile
+++ Dockerfile
@@ -16,5 +16,9 @@ COPY . .
 # Expose application port
 EXPOSE 3000
 
+HEALTHCHECK --interval=30s --timeout=5s --start-period=5s --retries=3 \
+  CMD wget -qO- http://localhost:3000/health || exit 1
+
+
 # Start application
 CMD ["npm","start"]
--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const MONGO_URL = process.env.MONGO_URL;
 
 // MongoDB connection
 mongoose.connect("mongodb://mongo:27017/devopsdb")
