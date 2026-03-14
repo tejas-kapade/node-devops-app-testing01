e1d3e18ece3caf2f33b4fc8bf4ed57adae41d7d0
Tejas Kapade <me.tejaskapade@gmail.com>
2026-03-14 16:27:38 +0530

    Edited docker build script to use ./script/deploy.sh for server

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,8 @@ docker stop node-devops-app-p1 || true
 docker rm node-devops-app-p1 || true
 
 docker run -d \
--p 3000:3000 \
+-p 80:3000 \
 --name node-devops-app \
 $DOCKER_USERNAME/node-devops-app-p1:latest
+
+docker image prune -af
